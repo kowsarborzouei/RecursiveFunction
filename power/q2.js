2a7494e555fbaca18c3ea6119a15e7a4b822b8f8
kowsar <k.borzuei@gmail.com>
2021-11-03 20:50:05 +0330

    q2 commit

---
+++ power/q2.js
@@ -0,0 +1,9 @@
+const power = (base, exponent) => {
+    if(typeof base != 'number'||typeof exponent != 'number') return ('please enter number! :)')
+    if (exponent !== 0) {
+        return base * power(base, exponent - 1);
+    } else {
+        return 1;
+    }
+}
+console.log(power(3,2))
